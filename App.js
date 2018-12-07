c91f20b39b0fb738f158d7f024d4ae8743e63c9d
Michelot Alexis <amichelot@club-internet.fr>
2018-12-07 09:52:27 +0100

    With xavier touch

--- App.js
+++ App.js
@@ -2,32 +2,96 @@ const express = require("express");
 const http = require("http");
 const WebSocket = require("ws");
 const uid2 = require("uid2");
-
+const mongoose = require("mongoose");
+const _ = require("lodash");
+mongoose.connect(
+  "mongodb://localhost:27017/chat-bicycly",
+  {
+    useNewUrlParser: true
+  },
+  function(err) {
+    if (err) console.error("Could not connect to mongodb.");
+  }
+);
 const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
 
-app.use(function(req, res) {
-  res.send({ msg: "hello" });
+// app.use(function(req, res) {
+//   res.send({ msg: "hello" });
+// });
+
+const UserModel = mongoose.model("User", {
+  name: String,
+  messages: [
+    {
+      _id: String,
+      createdAt: { type: Date, default: Date.now },
+      text: String,
+      user: {
+        _id: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User"
+        }
+      }
+    }
+  ]
+});
+
+app.post("/sign_up", function(req, res) {
+  const newUser = new UserModel(req.body);
+  newUser.save(function(err, createdUser) {
+    if (err) {
+      res.json({ error: err.message });
+    } else {
+      res.json(createdUser);
+    }
+  });
 });
 
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
 wss.on("connection", function connection(ws, req) {
+  console.log("incoming connexion");
   ws.on("message", function incoming(message) {
+    console.log("incoming message");
     try {
       const dataJSON = JSON.parse(message);
-      console.log(dataJSON);
-
+      console.log("dataJSON", dataJSON);
+      // Save message for senderId and receiverId here
+      UserModel.findOne({ _id: dataJSON._id }).exec(function(err, res) {
+        if (err) {
+          return res.json({ error: err.message });
+        } else {
+          console.log("user", res);
+          console.log(res.messages);
+          console.log("ESPECE D'ABRUTI", dataJSON);
+          res.messages.push(dataJSON);
+          res.save(function(err, savedMessage) {
+            console.log("message sauvegardé", savedMessage);
+          });
+        }
+      });
       wss.clients.forEach(function each(client) {
         if (client !== ws && client.readyState === WebSocket.OPEN) {
-          if (dataJSON.text && dataJSON.name) {
+          if (dataJSON.text && dataJSON._id) {
             client.send(
-              JSON.stringify({
+              /* JSON.stringify({
                 _id: uid2(10),
                 text: dataJSON.text,
                 user: { name: dataJSON.name }
+              }) */
+              JSON.stringify({
+                text: dataJSON.text,
+                _id: dataJSON._id,
+                user: {
+                  _id: dataJSON.user._id
+                }
               })
             );
+          } else {
+            console.log("fuck that shit");
           }
         }
       });
